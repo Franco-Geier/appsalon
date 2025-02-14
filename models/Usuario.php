5d0521a9ba14ef73de1882972434e1cec8f94508
Franco Geier <francoyogeier@gmail.com>
2025-02-14 16:08:43 -0300

    Validacion al login

--- models/Usuario.php
+++ models/Usuario.php
@@ -53,6 +53,18 @@ class Usuario extends ActiveRecord {
         return self::$alertas;
     }
 
+
+    public function validarLogin() {
+        if(!$this->email) {
+            self::$alertas["error"][] = "El E-mail es obligatorio";
+        }
+        if(!$this->password) {
+            self::$alertas["error"][] = "El password es obligatorio";
+        }
+        return self::$alertas;
+    }
+
+
     // Revisa si el usuario ya existe
     public function existeUsuario() {
         $query = " SELECT * FROM " . self:: $tabla . " WHERE email = '" . $this->email . "' LIMIT 1";
--- views/auth/login.php
+++ views/auth/login.php
@@ -1,7 +1,9 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia sesión con tus datos</p>
 
-<form class="formulario" method="POST" action="/">
+<?php include_once __DIR__ . "/../templates/alertas.php"; ?>
+
+<form class="formulario" method="POST" action="./">
     <div class="campo">
         <label for="email">Email</label>
         <input type="email" name="email" id="email" placeholder="Tu E-mail" autocomplete="email">
