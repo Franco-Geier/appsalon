da7ceb6a892888b0b159429ef72e7cee2c168709
Franco Geier <francoyogeier@gmail.com>
2025-02-18 22:07:39 -0300

    Finalizando navegación con tabs

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,32 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function iniciarApp() {
-    tabs();
+    mostrarSeccion(); // Muestra y oculta las secciones
+    tabs(); // Cambia la sección cuando se presionen los tabs
 }
 
 
 function mostrarSeccion() {
-    console.log("Mostrando seccion...");
+    // Ocultar la seccion que tenga la clase de mostrar
+    const seccionAnterior = document.querySelector(".mostrar");
+    if(seccionAnterior) {
+        seccionAnterior.classList.remove("mostrar");
+    }
+    
+    // Seleccionar la seccion con el paso...
+    const pasoSelector = `#paso-${paso}`;
+    const seccion = document.querySelector(pasoSelector);
+    seccion.classList.add("mostrar");
+
+    // Quita la clase actual al tab anterior
+    const tabAnterior = document.querySelector(".actual");
+    if(tabAnterior) {
+        tabAnterior.classList.remove("actual");
+    }
+
+    // Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add("actual");
 }
 
 
