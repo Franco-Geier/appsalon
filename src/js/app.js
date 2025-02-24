f9ef8ff7f910726cd0a75503149fedf72d2b5e2e
Franco Geier <francoyogeier@gmail.com>
2025-02-24 16:24:14 -0300

    Guardando citas con Fetch

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ const pasoInicial = 1;
 const pasoFinal = 3;
 
 const cita = {
+    id: "",
     nombre: "",
     fecha: "",
     hora: "",
@@ -23,6 +24,7 @@ function iniciarApp() {
 
     consultarAPI(); // Consulta la API en el backend de PHP
     
+    idCliente();
     nombreCliente(); // Añade el nombre del cliente al objeto de cita
     seleccionarFecha(); // Añade la fecha de la cita en el objeto
     seleccionarHora(); // Añade la hora de la cita en el objeto
@@ -165,6 +167,12 @@ function seleccionarServicio(servicio) {
         cita.servicios = [...servicios, servicio];
         divServicio.classList.add("seleccionado");
     }
+}
+
+
+function idCliente() {
+    cita.id = document.querySelector("#id").value;
+    
 }
 
 
@@ -328,13 +336,13 @@ function mostrarResumen() {
 
 
 async function reservarCita() {
-    const {nombre, fecha, hora, servicios} = cita;
+    const {nombre, fecha, hora, servicios, id} = cita;
     const idServicios = servicios.map(servicio => servicio.id);
 
     const datos = new FormData();
-    datos.append("nombre", nombre);
     datos.append("fecha", fecha);
     datos.append("hora", hora);
+    datos.append("usuarioId", id);
     datos.append("servicios", idServicios);
 
     // console.log([...datos]);
--- views/cita/index.php
+++ views/cita/index.php
@@ -33,6 +33,7 @@
                 <label for="hora">Hora</label>
                 <input type="time" name="hora" id="hora">
             </div>
+            <input type="hidden" id="id" value="<?php echo $id; ?>">
         </form>
     </div>
 
