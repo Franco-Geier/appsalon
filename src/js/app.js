5699736517b1fd3a4463dc9ae8f5805fb21cd451
Franco Geier <francoyogeier@gmail.com>
2025-02-24 16:09:04 -0300

    Creando modelo de Cita

--- src/js/app.js
+++ src/js/app.js
@@ -327,6 +327,27 @@ function mostrarResumen() {
 }
 
 
-function reservarCita() {
-    console.log("Reservando Cita");
+async function reservarCita() {
+    const {nombre, fecha, hora, servicios} = cita;
+    const idServicios = servicios.map(servicio => servicio.id);
+
+    const datos = new FormData();
+    datos.append("nombre", nombre);
+    datos.append("fecha", fecha);
+    datos.append("hora", hora);
+    datos.append("servicios", idServicios);
+
+    // console.log([...datos]);
+
+    // Petición hacia la api
+    const url = "http://localhost/appsalon/public/api/citas";
+
+    const respuesta = await fetch(url ,{
+        method: "POST",
+        body: datos
+    });
+
+    const resultado = await respuesta.json();
+    console.log(resultado);
+
 }
