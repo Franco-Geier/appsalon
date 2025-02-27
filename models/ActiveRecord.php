87b1f9a527a12f564ebca547c3459dff6e04aa6e
Franco Geier <francoyogeier@gmail.com>
2025-02-27 15:46:22 -0300

    Consulta plana a la BD

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -130,6 +130,14 @@ class ActiveRecord {
         return array_shift( $resultado ) ;
     }
 
+
+    // Consulta plana de SQL (Utilizar cuando los métodos del modelo no son suficientes)
+    public static function SQL($query) {
+        $resultado = self::consultarSQL($query);
+        return $resultado;
+    }
+    
+
     // crea un nuevo registro
     public function crear() {
         // Sanitizar los datos
