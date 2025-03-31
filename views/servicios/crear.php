b19f117bcfcc99d0f4d44187f9203229a00579c8
Franco-geier <francoyogeier@gmail.com>
2025-03-31 14:19:52 -0300

    Validando servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -2,10 +2,11 @@
 <p class="descripcion-pagina">Llena Todos Los Campos Para Añadir un Nuevo Servicio</p>
 
 <?php
-    include_once __DIR__ . "/../templates/barra.php";
+    // include_once __DIR__ . "/../templates/barra.php";
+    include_once __DIR__ . "/../templates/alertas.php";
 ?>
 
-<form method="post" class= "contenedor">
+<form action="/appsalon/public/servicios/crear" method="post" class= "contenedor">
     <?php include_once __DIR__ . "/formulario.php"; ?>
     <input type="submit" value="Guardar Servicio" class="boton">
 </form>
